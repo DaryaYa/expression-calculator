c097d7b7ff611944613f8e9a2e76eac92e188586
DaryaYa <lyalyaka@mail.ru>
2020-03-03 03:44:18 +0300

    nochbla

--- src/index.js
+++ src/index.js
@@ -4,7 +4,45 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    if (expr.length == 0) {return false};
+    
+//    let str = expr.match(/\(.+?\)/g).map(function(str) {    
+//         return str.slice(1,-1)
+//     })
+
+    let arr = expr.split('');
+
+   if (arr.filter(item => item =='(' || item ==')').length % 2 !== 0) {
+
+       throw new Error();
+   } 
+
+   function findBrackets(expr) {
+    if (expr.includes('(')) {
+        let startInd = expr.lastIndexOf('(');
+        let endInd = expr.indexOf(')');
+        let brackets = expr.slice(startInd+1, endInd);
+        let bracketsRes = 'blabla';
+
+        expr = expr.replace(`(${brackets})`, bracketsRes);
+        console.log(expr);
+        return findBrackets(expr);
+
+    } else {
+       return console.log(expr);
+    }
+   
+    return 
+   }
+   
+   
+//    arr.map(item => {
+
+// if (!Number(item)) {item = item} else { item = Number(item)}
+// return item;
+//     });
+
 }
 
 module.exports = {
